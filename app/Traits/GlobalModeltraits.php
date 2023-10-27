026ffef539db7e824dc34f2d31b8d6f0a44cf678
winex01 <winnie131212592@gmail.com>
2023-10-27 19:48:01 +0800

    add laravel soft deletes

--- app/Traits/GlobalModeltraits.php
+++ app/Traits/GlobalModeltraits.php
@@ -5,10 +5,12 @@ namespace App\Traits;
 use App\Traits\LogsActivity;
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 trait GlobalModelTraits
 {
     use CrudTrait; 
     use HasFactory;
     use LogsActivity;
+    use SoftDeletes;
 }
